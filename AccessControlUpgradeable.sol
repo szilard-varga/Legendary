94c533ab81e359b9a39291673d7a37b94ab69ee9
Szilard Varga <136651660+szilard-varga@users.noreply.github.com>
2024-01-18 09:49:16 +0200

    Update AccessControlUpgradeable.sol

--- AccessControlUpgradeable.sol
+++ AccessControlUpgradeable.sol
@@ -41,28 +41,16 @@ abstract contract AccessControlUpgradeable is Initializable, ContextUpgradeable,
     /**
      * @dev Modifier that checks that an account has a specific role. Reverts
      * with a standardized message including the required role.
-     *
-     * The format of the revert reason is given by the following regular expression:
-     *
-     *  /^AccessControl: account (0x[0-9a-f]{40}) is missing role (0x[0-9a-f]{64})$/
-     *
-     * _Available since v4.1._
      */
     modifier onlyRole(bytes32 role) {
         _checkRole(role);
         _;
     }
 
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
         return interfaceId == type(IAccessControlUpgradeable).interfaceId || super.supportsInterface(interfaceId);
     }
 
-    /**
-     * @dev Returns `true` if `account` has been granted `role`.
-     */
     function hasRole(bytes32 role, address account) public view virtual override returns (bool) {
         return _roles[role].members[account];
     }
@@ -70,10 +58,6 @@ abstract contract AccessControlUpgradeable is Initializable, ContextUpgradeable,
     /**
      * @dev Revert with a standard message if `_msgSender()` is missing `role`.
      * Overriding this function changes the behavior of the {onlyRole} modifier.
-     *
-     * Format of the revert message is described in {_checkRole}.
-     *
-     * _Available since v4.6._
      */
     function _checkRole(bytes32 role) internal view virtual {
         _checkRole(role, _msgSender());
