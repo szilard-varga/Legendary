16a6c3d51aa5c5379919af3e03a6d706e97d1c1f
Szilard Varga <136651660+szilard-varga@users.noreply.github.com>
2024-01-19 12:45:03 +0200

    Update EnumerableSetUpgradeable.sol

--- EnumerableSetUpgradeable.sol
+++ EnumerableSetUpgradeable.sol
@@ -14,15 +14,6 @@ pragma solidity ^0.8.0;
  * (O(1)).
  * - Elements are enumerated in O(n). No guarantees are made on the ordering.
  *
- * ```
- * contract Example {
- *     // Add the library methods
- *     using EnumerableSet for EnumerableSet.AddressSet;
- *
- *     // Declare a set state variable
- *     EnumerableSet.AddressSet private mySet;
- * }
- * ```
  *
  * As of v3.3.0, sets of type `bytes32` (`Bytes32Set`), `address` (`AddressSet`)
  * and `uint256` (`UintSet`) are supported.
