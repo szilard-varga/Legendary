42bfa64104d19aa43717bf19bbd9d23ca5073de3
Szilard Varga <136651660+szilard-varga@users.noreply.github.com>
2024-02-01 11:40:44 +0200

    Update ERC165Upgradeable.sol

--- ERC165Upgradeable.sol
+++ ERC165Upgradeable.sol
@@ -11,14 +11,6 @@ import "../../proxy/utils/Initializable.sol";
  *
  * Contracts that want to implement ERC165 should inherit from this contract and override {supportsInterface} to check
  * for the additional interface id that will be supported. For example:
- *
- * ```solidity
- * function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
- *     return interfaceId == type(MyInterface).interfaceId || super.supportsInterface(interfaceId);
- * }
- * ```
- *
- * Alternatively, {ERC165Storage} provides an easier to use but more expensive implementation.
  */
 abstract contract ERC165Upgradeable is Initializable, IERC165Upgradeable {
     function __ERC165_init() internal onlyInitializing {
